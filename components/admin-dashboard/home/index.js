5382a519f65d015b8f23000f02d38e097c8c932e
codingdrizzle <codingfrancis100@gmail.com>
2022-07-30 08:50:44 +0000

    worked on some parts of the dashboard

--- components/admin-dashboard/home/index.js
+++ components/admin-dashboard/home/index.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import { Row, Col } from 'antd'
 import Layout from "../../../layouts/admin-dashboard-layouts";
+import { AnalyticsCard, UsersCard } from './card';
+import { BsStack } from 'react-icons/bs'
+import { IoToday } from 'react-icons/io5'
+import { FaCalendarAlt, FaUserCheck } from 'react-icons/fa'
+
+
 
 
 const Home = () => {
     return (
         <Layout title={'Dashboard'}>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque quae sit, blanditiis maxime amet quas nisi libero cupiditate, architecto consequatur laboriosam porro sequi molestiae aspernatur sunt error dolor perspiciatis natus!
-            lorem1000
+            {/* Render all cards */}
+            <Row gutter={[20,10]}>
+                <Col xs={12} sm={8} md={6}>
+                    <AnalyticsCard cardBg={'rgba(27,202,205,0.3)'} textColor={'rgb(20,100,255)'} percent={70} typeText={'ALL'} icon={<BsStack size={20} color={'#00115B'}/>} typeNumber={'6,320'}/>
+                </Col>
+                <Col xs={12} sm={8} md={6}>
+                    <AnalyticsCard cardBg={'rgba(251,181,0,0.3)'} textColor={'rgb(255,150,0)'} percent={42} typeText={'MONTH'} icon={<FaCalendarAlt size={20} color={'#00115B'}/>} typeNumber={'3,998'}/>
+                </Col>
+                <Col xs={12} sm={8} md={6}>
+                    <AnalyticsCard cardBg={'rgba(233,36,36,0.3)'} textColor={'rgb(200,36,36)'} percent={10} typeText={'TODAY'} icon={<IoToday size={20} color={'#00115B'}/>} typeNumber={'562'}/>
+                </Col>
+                <Col xs={12} sm={8} md={6}>
+                    <UsersCard cardBg={'rgba(216, 216, 216, 0.3)'} textColor={'rgb(130, 130, 130)'} icon={<FaUserCheck size={20} color={'#00115B'}/>} typeText={'USERS'} typeNumber={'2,300'}/>
+                </Col>
+            </Row>
         </Layout>
     );
 }
--- layouts/admin-dashboard-layouts/index.js
+++ layouts/admin-dashboard-layouts/index.js
@@ -1,16 +1,22 @@
 import React from 'react'
-import { Row, Col, Typography } from 'antd'
+import { Row, Col, Button, Typography } from 'antd'
 import Navigation from './side-nav'
 import styles from '../../styles/admin-styles/layout-styles/side-nav.module.css'
-
-const { Title } = Typography
+import { FaSignOutAlt } from 'react-icons/fa'
+const { Title, Text } = Typography
 
 const Layout = ({ title, children }) => {
   return (
     <div style={{ display: 'flex', justifyContent: 'space-between', position: 'relative' }}>
       <Navigation />
       <Row className={styles.mainContent}>
-        <Col span={24}><Title>{title}</Title></Col>
+        <Col span={24} style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: 20 }}>
+          <Title style={{margin: 0}}>{title}</Title>
+          <Button className={styles.logoutBtn}>
+            <Text style={{ color: '#fff', fontWeight: 500 }} className={styles.logoutText}>Logout</Text>
+            <FaSignOutAlt size={20} />
+          </Button>
+        </Col>
         <Col span={24}>
           {children}
         </Col>
