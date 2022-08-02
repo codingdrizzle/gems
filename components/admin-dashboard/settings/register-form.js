0c969b903e585f1be1346d0fa56ea6541ee34597
codingdrizzle <codingfrancis100@gmail.com>
2022-08-02 12:18:33 +0000

    worked on admin settings/ new agent Form

--- components/admin-dashboard/settings/register-form.js
+++ components/admin-dashboard/settings/register-form.js
@@ -1,13 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaUser, FaLock, FaAddressCard } from 'react-icons/fa'
+import { MdEmail } from 'react-icons/md'
+import { Card, Row, Col, Button, Input, Typography } from 'antd'
+
 import styles from '../../../styles/admin-styles/settings-styles/register.module.css'
 
+const { Title } = Typography
+const { Password } = Input
+
 const RegisterForm = () => {
+  const [ name, setName ] = useState('')
+  const [ email, setEmail ] = useState('')
+  const [ username, setUsername ] = useState('')
+  const [ password, setPassword ] = useState('')
+
   return (
-    <div>
-      <div style={{width : '100%'}}>
-        REGISTER FORM
-      </div> 
-    </div>
+    <Card className={[styles.card, 'registerCard']}>
+      <Row justify='center' align='midde' gutter={[20,20]} className={styles.cardRow}>
+        <Col span={24} style={{ textAlign: 'center' }}><Title level={4} style={{ color: '#00115b', fontWeight: 600}}>Register Agent</Title></Col>
+        <Col span={24}>
+          <Input prefix={<FaAddressCard color='#91A2B8' size={16} style={{marginRight: 10}}/>} type={'text'} placeholder='Agent Name' value={name} className={styles.input} onChange={(e) => setName(e.target.value) }></Input>
+        </Col>
+        <Col span={24}>
+          <Input prefix={<MdEmail color='#91A2B8' size={16} style={{marginRight: 10}}/>} type={'email'} placeholder='Email Address' value={email} className={styles.input} onChange={(e) => setEmail(e.target.value)}></Input>
+        </Col>
+        <Col span={24}>
+          <Input prefix={<FaUser color='#91A2B8' size={16} style={{marginRight: 10}}/>} type={'text'} placeholder='Username' value={username} className={styles.input} onChange={(e) => setUsername(e.target.value)}></Input>
+        </Col>
+        <Col span={24}>
+          <Password prefix={<FaLock color='#91A2B8' size={16} style={{marginRight: 10}}/>} placeholder='Password' value={password} className={styles.input} onChange={(e) => setPassword(e.target.value)}></Password>
+        </Col>
+        <Col span={24} style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+          <Button className={styles.btn}>Register</Button>
+        </Col>
+      </Row>
+    </Card>
   )
 }
 
--- layouts/admin-dashboard-layouts/side-nav.js
+++ layouts/admin-dashboard-layouts/side-nav.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import Link from 'next/link'
+import Router from 'next/router'
 import { Typography } from 'antd'
 import Logo from '../../commons/logo'
 import styles from '../../styles/admin-styles/layout-styles/side-nav.module.css'
@@ -8,6 +9,7 @@ import { FaBell, FaPodcast } from 'react-icons/fa'
 import { IoSettings } from 'react-icons/io5'
 
 const { Text } = Typography
+const router = Router
 
 const handleClick = (e) => {
     console.log(e.target.classList.add('me'))
@@ -26,25 +28,25 @@ const Navigation = () => {
                 <div className={styles.sideNav} onMouseOver={handleHover}>
                     <div span={24} className={styles.logo}><Logo value={50} /></div>
                     <ul className={styles.navItems}>
-                        <li className={styles.navItem} onClick={handleClick}>
+                        <li className={styles.navItem} onClick={() => router.push('/admin')}>
                             <BsFillGridFill size={25} className={styles.navIcon} />
-                            <Link href={'/admin/'} className={styles.link}>Dashboard</Link>
+                            <Text className={styles.text}>Dashboard</Text>
                         </li>
-                        <li className={styles.navItem} onClick={handleClick}>
+                        <li className={styles.navItem} onClick={() => router.push('/admin/complaints')}>
                             <FaBell size={25} className={styles.navIcon} />
-                            <Link href={'/admin/complaints'} className={styles.link}>Complaints</Link>
+                            <Text className={styles.text}>Complaints</Text>
                         </li>
-                        <li className={styles.navItem} onClick={handleClick}>
+                        <li className={styles.navItem} onClick={() => router.push('/admin/statistics')}>
                             <BsBarChartLineFill size={25} className={styles.navIcon} />
-                            <Link href={'/admin/statistics'} className={styles.link}>Statistics</Link>
+                            <Text className={styles.text}>Statistics</Text>
                         </li>
-                        <li className={styles.navItem} onClick={handleClick}>
+                        <li className={styles.navItem} onClick={() => router.push('/admin/broadcast')}>
                             <FaPodcast size={25} className={styles.navIcon} />
-                            <Link href={'/admin/broadcast'} className={styles.link}>Broadcast</Link>
+                            <Text className={styles.text}>Broadcast</Text>
                         </li>
-                        <li className={styles.navItem} onClick={handleClick}>
+                        <li className={styles.navItem} onClick={() => router.push('/admin/settings')}>
                             <IoSettings size={25} className={styles.navIcon} />
-                            <Link href={'/admin/settings'} className={styles.link}>Settings</Link>
+                            <Text className={styles.text}>Settings</Text>
                         </li>
                     </ul>
                 </div>
