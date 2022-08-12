9ee5afe1a19f6a88f604653f92eb3c64228c4779
codingdrizzle <codingfrancis100@gmail.com>
2022-08-12 12:30:48 +0000

    mangooase

--- utils/connect-mongo.js
+++ utils/connect-mongo.js
@@ -1,5 +1,11 @@
 import mongoose from "mongoose";
 
-const connect = async () => mongoose.connect(process.env.MONGODB_URL || process.env.DATABASE_URL)
+const connect = async () => mongoose.connect(
+    process.env.MONGODB_URL || process.env.DATABASE_URL,
+    {  },
+    () => {
+        console.log('Connected to MongoDB');
+    }
+)
 
 export default connect;
