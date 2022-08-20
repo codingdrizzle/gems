28f7dc2b95d294cf33f3f55005a0c90bc047371a
codingdrizzle <codingfrancis100@gmail.com>
2022-08-20 12:17:06 +0000

    environment variables

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const nextConfig = {
   images: {
     domains: ['res.cloudinary.com']
   },
-  staticPageGenerationTimeout: 1000,
 }
 
 module.exports = nextConfig
--- utils/connect-mongo.js
+++ utils/connect-mongo.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connect = async () => {
     mongoose.connect(
-        'mongodb+srv://codingdrizzle:password123@cluster0.qtomywv.mongodb.net/?retryWrites=true&w=majority' || process.env.DATABASE_URL,
+        process.env.MONGODB_URL || process.env.DATABASE_URL,
         {},
         () => {
             console.log('Connected to MongoDB');
