f789e14af4b0c749e6e6474b81c3260d6626fdbb
codingdrizzle <codingfrancis100@gmail.com>
2022-08-22 15:38:31 +0000

    ma head dey ache me

--- server/manager/expressManager.js
+++ server/manager/expressManager.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 class ExpressManager {
   constructor(nextApp) {
     this.instance = express();
-    this.instance = use(cors());
+    this.instance.use(cors());
     this.instance.use(express.json());
     this.instance.use(express.urlencoded());
     this.instance.use(nextApp.getRequestHandler());
