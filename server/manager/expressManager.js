47fa897396af5e82eed07a7928a89492a8b42e89
codingdrizzle <codingfrancis100@gmail.com>
2022-08-22 14:57:24 +0000

    add cors

--- server/manager/expressManager.js
+++ server/manager/expressManager.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require('cors')
 
 class ExpressManager {
   constructor(nextApp) {
     this.instance = express();
+    this.instance = use(cors());
     this.instance.use(express.json());
     this.instance.use(express.urlencoded());
     this.instance.use(nextApp.getRequestHandler());
