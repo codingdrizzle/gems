816c9a8093469f3830498a9de4a39ebeb54bc90a
codingdrizzle <codingfrancis100@gmail.com>
2022-08-27 13:41:42 +0000

    Tulenkey in the building

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt"
 
 export default async function middleware(req) {
-    const {token} = await getToken(req)
+    const token = await getToken({ req })
     console.log(token)
     const url = req.url
     if (url.includes('/user')) {
